1868766881340c9fe6c363720f48b2c359c74b98
Tyson Bowers <tkblink@gmail.com>
2014-10-16 02:25:41 -0400

    Javascript Class Assignments

---
+++ Chapter2/myFavMovie.js
@@ -0,0 +1,7 @@
+//Tyson Bowers
+//Write favorite movie description
+document.write("Forrest Gump is a movie of hope, destiny, and coincidence.<br>");
+document.write("Forrest is a man born with a physical handicap and a low I.Q.<br>");
+document.write("Forrest overcomes his dispositions to become very successful.<br>");
+document.write("Forrest seems fatefully tied to his Love Jenny and important events throughout history.<br>");
+document.write("Other times Forrest appears to be floating through life and coincidentally significant.<br>");
